880fb515ec21d86d2f0ba7ea62cef8077fa0d31b
juliasalek <julia_pytel@wp.pl>
2023-10-09 23:53:41 +0200

    Add missing modules

--- src/app/modules/human/human-battle/human-battle.component.spec.ts
+++ src/app/modules/human/human-battle/human-battle.component.spec.ts
@@ -1,18 +1,71 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HumanBattleComponent } from './human-battle.component';
+import { StarWarsApiService } from '../../../services';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Human } from '../../../interfaces';
+import { HumanCardComponent } from '../human-card';
+
+const testHuman2: Human = {
+  birth_year: '1990',
+  eye_color: 'blue',
+  films: ['Film 1', 'Film 2'],
+  gender: 'male',
+  hair_color: 'brown',
+  height: '175 cm',
+  homeworld: 'Earth',
+  mass: '70 kg',
+  name: 'John Doe',
+  skin_color: 'fair',
+  created: '2023-10-07T12:00:00Z',
+  edited: '2023-10-07T12:00:00Z',
+  species: ['Species 1', 'Species 2'],
+  starships: ['Starship 1', 'Starship 2'],
+  url: 'https://example.com/human/1',
+  vehicles: ['Vehicle 1', 'Vehicle 2'],
+};
+
+const testHuman: Human = {
+  birth_year: '1990',
+  eye_color: 'blue',
+  films: ['Film 1', 'Film 2'],
+  gender: 'male',
+  hair_color: 'brown',
+  height: '175 cm',
+  homeworld: 'Earth',
+  mass: '70 kg',
+  name: 'All Doe',
+  skin_color: 'fair',
+  created: '2023-10-07T12:00:00Z',
+  edited: '2023-10-07T12:00:00Z',
+  species: ['Species 1', 'Species 2'],
+  starships: ['Starship 1', 'Starship 2'],
+  url: 'https://example.com/human/1',
+  vehicles: ['Vehicle 1', 'Vehicle 2'],
+};
 
 describe('HumanBattleComponent', () => {
-  let component: HumanBattleComponent;
   let fixture: ComponentFixture<HumanBattleComponent>;
+  let component: HumanBattleComponent;
+  let apiService: jasmine.SpyObj<StarWarsApiService>;
 
   beforeEach(() => {
+    const apiServiceSpy = jasmine.createSpyObj('StarWarsApiService', [
+      'getHumanById',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [HumanBattleComponent]
+      declarations: [HumanBattleComponent],
+      imports: [MatCardModule, MatIconModule],
+      providers: [{ provide: StarWarsApiService, useValue: apiServiceSpy }],
     });
+
     fixture = TestBed.createComponent(HumanBattleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = TestBed.inject(
+      StarWarsApiService
+    ) as jasmine.SpyObj<StarWarsApiService>;
   });
 
   it('should create', () => {
--- src/app/modules/starships/starships-battle/starships-battle.component.spec.ts
+++ src/app/modules/starships/starships-battle/starships-battle.component.spec.ts
@@ -1,21 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatCardModule } from '@angular/material/card';
+import { StarWarsApiService } from '../../../services';
 import { StarshipsBattleComponent } from './starships-battle.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('StarshipsBattleComponent', () => {
-  let component: StarshipsBattleComponent;
   let fixture: ComponentFixture<StarshipsBattleComponent>;
+  let component: StarshipsBattleComponent;
+  let apiService: jasmine.SpyObj<StarWarsApiService>;
 
   beforeEach(() => {
+    const apiServiceSpy = jasmine.createSpyObj('StarWarsApiService', [
+      'getStarshipById',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [StarshipsBattleComponent]
+      declarations: [StarshipsBattleComponent],
+      imports: [MatCardModule, MatIconModule],
+      providers: [{ provide: StarWarsApiService, useValue: apiServiceSpy }],
     });
+
     fixture = TestBed.createComponent(StarshipsBattleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    apiService = TestBed.inject(
+      StarWarsApiService
+    ) as jasmine.SpyObj<StarWarsApiService>;
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
